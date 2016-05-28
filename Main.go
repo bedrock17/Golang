abec5ccc4ea2237a384dfe16ec61365719fa02fd
Cube <bedrook@gmail.com>
2016-05-28 22:47:06 +0900

    add Router

--- Main.go
+++ Main.go
@@ -1,25 +1,27 @@
 package main
 
-import("fmt")
+import(
+	"fmt"
+	"net/http"
+
+
+)
+
+
+
 
 func main() {
 
-	arr:=[10][10]int{}
-
-	for i:=0 ; i<10;i++{
-		for j:=0;j<10;j++{
-			arr[i][j]=i*j
-		}
-
-	}
-
-	for i:=0 ; i<10;i++{
-		for j:=0;j<10;j++{
-			fmt.Printf("%d ",arr[i][j])
-		}
-		fmt.Println()
-	}
-	fmt.Printf("Hello World")
 
 
+	fmt.Printf("Server Start")
+
+	http.HandleFunc("/",func(w http.ResponseWriter,r *http.Request){
+		fmt.Fprintf(w,"Welcome!")
+	})
+	http.HandleFunc("/about",func(w http.ResponseWriter,r *http.Request){
+		fmt.Fprintf(w,"about")
+	})
+
+	http.ListenAndServe(":8080",nil);
 }
